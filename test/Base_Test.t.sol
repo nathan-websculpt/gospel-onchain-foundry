7e060ad6be4ee78fbe878a63fc7b2e2086e87e97
nathan-websculpt <websculpt.nathan@gmail.com>
2025-04-25 12:41:42 -0500

    cleanup

--- test/Base_Test.t.sol
+++ test/Base_Test.t.sol
@@ -7,8 +7,10 @@ import {BookManager} from "../src/BookManager.sol";
 import {Test, console2} from "forge-std/Test.sol";
 
 abstract contract Base_Test is Test {
+    uint256 constant ARRAY_LEN = 20;
     BookDeployer _deployer;
     BookManager _manager;
+    BookDeployer.Deployment[] _books;
     address owner;
     address alice;
     // address bob;
@@ -19,93 +21,69 @@ abstract contract Base_Test is Test {
         // bob = address(0x2);
         _deployer = new BookDeployer(owner);
         _manager = new BookManager(0, "cloneable blank", owner);
+
+        _deployBook(1, "Book One");
     }
 
     function testDeployBook() public virtual {
-        _deployer.deployBook(1, "Book One");
-
-        BookDeployer.Deployment[] memory deployments = _deployer.getDeployments();
-
-        assertEq(deployments.length, 1);
+        assertEq(_books.length, 1);
     }
 
     function testStoreVerses() public virtual {
-        uint256 ARRAY_LEN = 10;
-        _deployer.deployBook(1, "Book One");
-
-        BookDeployer.Deployment[] memory deployments = _deployer.getDeployments();
-
-
-        uint256[] memory _verseNumbers = new uint256[](ARRAY_LEN);
-        uint256[] memory _chapterNumbers = new uint256[](ARRAY_LEN);
-        string[] memory _verseContent = new string[](ARRAY_LEN);
-
-        for (uint256 i = 0; i < ARRAY_LEN; i++) {
-            uint256 ip1 = i + 1;
-            _verseNumbers[i] = ip1;
-            _chapterNumbers[i] = 1;
-            _verseContent[i] = string(abi.encodePacked("TEST ", vm.toString(ip1)));
-        }
+        _deployBook(2, "Book Two");
 
         bytes memory _bookId = abi.encodePacked("0x1234567890abcdef");
 
-        _manager.addBatchVerses(_bookId, _verseNumbers, _chapterNumbers, _verseContent);
+        (uint256[] memory _verseNumbers, uint256[] memory _chapterNumbers, string[] memory _verseContent) = _makeVerses();
 
-        BookManager.VerseStr memory lastVerseAdded = _manager.getLastVerseAdded();
+        _manager.addBatchVerses(
+            _bookId,
+            _verseNumbers,
+            _chapterNumbers,
+            _verseContent
+        );
+
+        BookManager.VerseStr memory lastVerseAdded = _manager
+            .getLastVerseAdded();
 
         assertEq(lastVerseAdded.verseNumber, ARRAY_LEN);
-        assertEq(lastVerseAdded.verseContent, "TEST 10");
+        assertEq(lastVerseAdded.verseContent, "TEST 20");
     }
 
     function testGetLastVerseAdded() public virtual {
-        uint256 ARRAY_LEN = 30;
-        _deployer.deployBook(1, "Book One");
+        _deployBook(2, "Book Two");       
 
-        BookDeployer.Deployment[] memory deployments = _deployer.getDeployments();
+        bytes memory _bookId = abi.encodePacked("0x1234567890abcdef"); 
 
+        (uint256[] memory _verseNumbers, uint256[] memory _chapterNumbers, string[] memory _verseContent) = _makeVerses();
 
-        uint256[] memory _verseNumbers = new uint256[](ARRAY_LEN);
-        uint256[] memory _chapterNumbers = new uint256[](ARRAY_LEN);
-        string[] memory _verseContent = new string[](ARRAY_LEN);
+        _manager.addBatchVerses(
+            _bookId,
+            _verseNumbers,
+            _chapterNumbers,
+            _verseContent
+        );
 
-        for (uint256 i = 0; i < ARRAY_LEN; i++) {
-            uint256 ip1 = i + 1;
-            _verseNumbers[i] = ip1;
-            _chapterNumbers[i] = 1;
-            _verseContent[i] = string(abi.encodePacked("TEST ", vm.toString(ip1)));
-        }
-
-        bytes memory _bookId = abi.encodePacked("0x1234567890abcdef");
-
-        _manager.addBatchVerses(_bookId, _verseNumbers, _chapterNumbers, _verseContent);
-
-        BookManager.VerseStr memory lastVerseAdded = _manager.getLastVerseAdded();
+        BookManager.VerseStr memory lastVerseAdded = _manager
+            .getLastVerseAdded();
 
         assertEq(lastVerseAdded.verseNumber, ARRAY_LEN);
-        assertEq(lastVerseAdded.verseContent, "TEST 30");
+        assertEq(lastVerseAdded.verseContent, "TEST 20");
     }
 
     function testGetVerseByNumber() public virtual {
-        uint256 ARRAY_LEN = 20;
-        _deployer.deployBook(1, "Book One");
-
-        BookDeployer.Deployment[] memory deployments = _deployer.getDeployments();
-
-
-        uint256[] memory _verseNumbers = new uint256[](ARRAY_LEN);
-        uint256[] memory _chapterNumbers = new uint256[](ARRAY_LEN);
-        string[] memory _verseContent = new string[](ARRAY_LEN);
-
-        for (uint256 i = 0; i < ARRAY_LEN; i++) {
-            uint256 ip1 = i + 1;
-            _verseNumbers[i] = ip1;
-            _chapterNumbers[i] = 1;
-            _verseContent[i] = string(abi.encodePacked("TEST ", vm.toString(ip1)));
-        }
+        _deployBook(2, "Book Two");
 
         bytes memory _bookId = abi.encodePacked("0x1234567890abcdef");
 
-        _manager.addBatchVerses(_bookId, _verseNumbers, _chapterNumbers, _verseContent);
+        (uint256[] memory _verseNumbers, uint256[] memory _chapterNumbers, string[] memory _verseContent) = _makeVerses();
+
+        _manager.addBatchVerses(
+            _bookId,
+            _verseNumbers,
+            _chapterNumbers,
+            _verseContent
+        );
 
         BookManager.VerseStr memory firstVerse = _manager.getVerseByNumber(1);
 
@@ -118,10 +96,39 @@ abstract contract Base_Test is Test {
         assertEq(anotherTest.verseContent, "TEST 11");
     }
 
+    // HELPERS
+
+    // sets up a new book, gets all deployments afterwards
+    function _deployBook(uint256 _bookId, string memory _bookName) private {
+        _deployer.deployBook(_bookId, _bookName);
+        _books = _deployer.getDeployments();
+    }
+
+    function _makeVerses()
+        private
+        returns (uint256[] memory, uint256[] memory, string[] memory)
+    {
+        uint256[] memory _verseNumbers = new uint256[](ARRAY_LEN);
+        uint256[] memory _chapterNumbers = new uint256[](ARRAY_LEN);
+        string[] memory _verseContent = new string[](ARRAY_LEN);
+
+        for (uint256 i = 0; i < ARRAY_LEN; i++) {
+            uint256 ip1 = i + 1;
+            _verseNumbers[i] = ip1;
+            _chapterNumbers[i] = 1;
+            _verseContent[i] = string(
+                abi.encodePacked("TEST ", vm.toString(ip1))
+            );
+        }
+
+        return (_verseNumbers, _chapterNumbers, _verseContent);
+    }
+    // END: HELPERS
+
     // tests:
     //          confirmVerse
     //          finalizeBook
-    //          
+    //
     //          PRIVATES:
     //                      preventSkippingVerse
     //                      preventSkippingChapter
